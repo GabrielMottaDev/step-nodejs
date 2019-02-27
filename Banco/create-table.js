a10e50b0d75e173d24079a5ed255dc762e292d38
Gabriel Motta <gabrielmottadev@gmail.com>
2019-02-27 20:57:14 -0300

    New class work

--- Banco/create-table.js
+++ Banco/create-table.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
     host : 'localhost',
-    port : 3306,
+    port : 8080,
     user : 'root',
-    password : 'root',
+    password : '',
     database : 'GabrielMottaConn'
 });
 
@@ -12,17 +12,28 @@ connection.connect(
         if(err) return console.log(err);
         console.log('Conectado');
         createTable(connection);
+        addAluno(connection);
     }
 );
 
 function createTable(conn) {
-    const sql = "CREATE TABLE IF NOT EXISTS `aluno` (";
-    sql += "`id` INT NOT NULL AUTO_INCREMENT, ";
-    sql += "`nome` NCHAR(20) NOT NULL, ";
-    sql += "`idade` INT NOT NULL, ";
-    sql += ");";
+    const sql = "CREATE TABLE IF NOT EXISTS `aluno` ("+
+                    "`id` INT NOT NULL AUTO_INCREMENT, "+
+                    "`nome` NCHAR(20) NOT NULL, "+
+                    "`cpf` VARCHAR(11) NOT NULL, "+
+                    "PRIMARY KEY (`id`)"+
+                ");";
     conn.query(sql, function (error, result, fields) {
         if (error) return console.log(error);
         console.log('Tabela criada com sucesso');
+    });
+}
+
+function addAluno(conn){
+    const sql = "INSERT INTO `aluno` (`nome`, `cpf`) "+
+                "VALUES ('Zeca', '00987654321');";
+    conn.query(sql, function (error, results, fields){
+        if(error) return console.log(error);
+        console.log('Aluno criado com sucesso!');
     });
 }
